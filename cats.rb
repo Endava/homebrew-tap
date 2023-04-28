acd5b39d8d7a6ef37a6029a5d0e0be363a06b453
Madalin Ilie <madalin.ilie@endava.com>
2023-04-28 23:05:33 +0300

    Update to the latest version of CATS

--- cats.rb
+++ cats.rb
@@ -4,8 +4,8 @@
 class Cats < Formula
   desc "CATS is a REST APIs fuzzer and negative testing tool for OpenAPI endpoints. CATS automatically  generates, runs and reports tests with minimum configuration and no coding effort. Tests are self-healing and do not require maintenance."
   homepage ""
-  url "https://github.com/Endava/cats/releases/download/cats-8.6.0/cats_macos_amd64_8.6.0.tar.gz"
-  sha256 "dfa4bfe2827a74acb79adad18a9a8c13b9077e6cc6984b35a810305eb5d6b9cc"
+  url "https://github.com/Endava/cats/releases/download/cats-8.6.1/cats_macos_amd64_8.6.1.tar.gz"
+  sha256 "b6f4c5679ff700623881380595fe3093d1c82629820b9272357423612637023b"
   license "Apache-2.0"
 
   # depends_on "cmake" => :build
