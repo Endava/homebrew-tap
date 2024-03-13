4b3c1b75f9b0c239d0631caec6c32a656b80a3e1
Madalin Ilie <madalin.ilie@endava.com>
2024-03-13 21:44:33 +0200

    Update to latest version of cats

--- cats.rb
+++ cats.rb
@@ -7,22 +7,22 @@ class Cats < Formula
 
   if OS.mac?
       if Hardware::CPU.intel?
-        url "https://github.com/Endava/cats/releases/download/cats-11.1.0/cats_macos_amd64_11.1.0.tar.gz"
+        url "https://github.com/Endava/cats/releases/download/cats-11.2.0/cats_macos_amd64_11.2.0.tar.gz"
         sha256 ""
       elsif Hardware::CPU.arm?
-        url "https://github.com/Endava/cats/releases/download/cats-11.1.0/cats_macos_arm64_11.1.0.tar.gz"
-        sha256 "4b2cf24a24bc53c327c7cc1d2d76c6456ed4fc63d99958cf969c6486b6a21355"
+        url "https://github.com/Endava/cats/releases/download/cats-11.2.0/cats_macos_arm64_11.2.0.tar.gz"
+        sha256 "57bc5c44a245d3e9dfcc659f815c830a3138e660d396fa8d4bc5d0e5da2fb0da"
       end
   elsif OS.linux?
       if Hardware::CPU.intel?
-        url "https://github.com/Endava/cats/releases/download/cats-11.1.0/cats_linux_amd64_11.1.0.tar.gz"
-        sha256 "5210a4d74de6309c9fe6f4386e3d143314bdc9a991dcf516cc06eeafe0055007"
+        url "https://github.com/Endava/cats/releases/download/cats-11.2.0/cats_linux_amd64_11.2.0.tar.gz"
+        sha256 "11c67d17068d0734d4dc674f0e47ed11e7401e1f8090945e174fbae269be1973"
       elsif Hardware::CPU.arm?
-        url "https://github.com/Endava/cats/releases/download/cats-11.1.0/cats_linux_arm64_11.1.0.tar.gz"
-        sha256 "ae07a71ca268af2c474d627005ba96088fe401da8d4db8ae241de9cd7ba4ad40"
+        url "https://github.com/Endava/cats/releases/download/cats-11.2.0/cats_linux_arm64_11.2.0.tar.gz"
+        sha256 "2e965780a6427d66c41a4325ef37687d4eade61e3728294e96b8c8cf51cc0fb7"
       end
   else
-    odie "Please use the uberjar version: https://github.com/Endava/cats/releases/download/cats-11.1.0/cats_uberjar_11.1.0.tar.gz"
+    odie "Please use the uberjar version: https://github.com/Endava/cats/releases/download/cats-11.2.0/cats_uberjar_11.2.0.tar.gz"
   end
 
   license "Apache-2.0"
