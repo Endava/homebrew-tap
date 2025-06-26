755a30f7359a9c0cc2abab4f5e16445eb87aab78
Madalin Ilie <madalin.ilie@endava.com>
2025-06-26 08:52:02 +0300

    Update to latest version of cats

--- cats.rb
+++ cats.rb
@@ -7,22 +7,22 @@ class Cats < Formula
 
   if OS.mac?
       if Hardware::CPU.intel?
-        url "https://github.com/Endava/cats/releases/download/cats-13.2.0/cats_macos_amd64_13.2.0.tar.gz"
-        sha256 "97a5fa6553d1ec4323dc42e9cca839f45ea1202df6f975aa5cfc089c7e6db470"
+        url "https://github.com/Endava/cats/releases/download/cats-13.3.2/cats_macos_amd64_13.3.2.tar.gz"
+        sha256 "79ef552c9656fa4134a79592568722cdbcb5b2cf4b2d28e3f3a9d2acbb7cc1e4"
       elsif Hardware::CPU.arm?
-        url "https://github.com/Endava/cats/releases/download/cats-13.2.0/cats_macos_arm64_13.2.0.tar.gz"
-        sha256 "51df0ce322e5077f0d38df058605b3ff16477657833852d38c46aca1ee37e2ba"
+        url "https://github.com/Endava/cats/releases/download/cats-13.3.2/cats_macos_arm64_13.3.2.tar.gz"
+        sha256 "ddf7b4a9ba85e46b1680a2359bd22ee9248966078c49a1ff049ad12b809e8be1"
       end
   elsif OS.linux?
       if Hardware::CPU.intel?
-        url "https://github.com/Endava/cats/releases/download/cats-13.2.0/cats_linux_amd64_13.2.0.tar.gz"
-        sha256 "4837e5c158d6c975dd8e02e6f8c09915a50a5200a58f5c02ba2080bdaf63abd1"
+        url "https://github.com/Endava/cats/releases/download/cats-13.3.2/cats_linux_amd64_13.3.2.tar.gz"
+        sha256 "bfd0486acff52fcc6f25d2bbeb3b5f4e4429f6871a4aff1133aa843727b154ee"
       elsif Hardware::CPU.arm?
-        url "https://github.com/Endava/cats/releases/download/cats-13.2.0/cats_linux_arm64_13.2.0.tar.gz"
-        sha256 "e504683577b2ffeedd9e80471deec59db63a5101a8ca0306071552d32acc2388"
+        url "https://github.com/Endava/cats/releases/download/cats-13.3.2/cats_linux_arm64_13.3.2.tar.gz"
+        sha256 "99a794ebf41c1646195dab191e96a69d775e6513f27a03f4316a143b011f0f7e"
       end
   else
-    odie "Please use the uberjar version: https://github.com/Endava/cats/releases/download/cats-13.2.0/cats_uberjar_13.2.0.tar.gz"
+    odie "Please use the uberjar version: https://github.com/Endava/cats/releases/download/cats-13.3.2/cats_uberjar_13.3.2.tar.gz"
   end
 
   license "Apache-2.0"
